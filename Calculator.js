9803b748dc12c30ead6e032bf9da28a9bd4f08ab
Tiago <thiagowendtpercyak@outlook.com>
2023-07-25 18:36:31 -0300

    calculadora usando tabela

---
+++ Calculator.js
@@ -0,0 +1,7 @@
+const calculadora = document.querySelector(".calculadora");
+const keys = document.querySelector(".cbutton");
+
+keys.addEventListener("click", (e) => {
+  if ((e.target, maches("button"))) {
+  }
+});
