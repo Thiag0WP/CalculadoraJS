2d473506dc31676e4f21aecf46890fbb1b67114b
Tiago <thiagowendtpercyak@outlook.com>
2023-07-26 17:17:12 -0300

    26/07/2023 17:15

--- Calculator.js
+++ Calculator.js
@@ -1,7 +1,27 @@
-const calculadora = document.querySelector(".calculadora");
-const keys = document.querySelector(".cbutton");
+function insert(num) {
+  var numero = document.getElementById("display").innerHTML;
+  document.getElementById("display").innerHTML = numero + num;
+}
 
-keys.addEventListener("click", (e) => {
-  if ((e.target, maches("button"))) {
+function clean() {
+  document.getElementById("display").innerHTML = "";
+}
+
+function back() {
+  var display = document.getElementById("display").innerHTML;
+  document.getElementById("display").innerHTML = display.substring(
+    0,
+    display.length - 1
+  );
+}
+function calcular() {
+  var display = document.getElementById("display").innerHTML;
+  if (display) {
+    if (display.includes("/0")) {
+      alert("Buuum, não é possivel dividir por zero!");
+      clean();
+    } else {
+      document.getElementById("display").innerHTML = eval(display);
+    }
   }
-});
+}
